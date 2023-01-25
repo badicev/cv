405f8640887213834b6bea04a6ae63d6ddcd519f
Başak Dilara Çevik <91937187+badicev@users.noreply.github.com>
2023-01-25 19:30:50 +0300

    Update visitcounter.js

--- scripts/visitcounter.js
+++ scripts/visitcounter.js
@@ -1,5 +1,5 @@
-const getCounterUrl = "https://comtrsitefunctions.azurewebsites.net/api/visitcounter/badicev/GetVisitCount";
-const addCounterUrl = "https://comtrsitefunctions.azurewebsites.net/api/visitcounter/badicev/AddVisitCount";
+const getCounterUrl = "https://comtrvisitcounter.onrender.com/api/visitcounter/badicev/GetVisitCount";
+const addCounterUrl = "https://comtrvisitcounter.onrender.com/api/visitcounter/badicev/AddVisitCount";
 
 
 function Run() {
